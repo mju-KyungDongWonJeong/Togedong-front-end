69fe6338623b39f4cf001ef9a0ed6a44a054e671
gyuhyuk <bruno3@naver.com>
2023-10-16 02:57:30 +0900

    feat: MediumButton Component 구현 및 event 구현

--- src/assets/component/SmallButton.tsx
+++ src/assets/component/SmallButton.tsx
@@ -1,26 +1,37 @@
 import styled from 'styled-components';
 
 /**
- * 사용하실 때 <SmallButton text="챌린지" /> 형식으로 사용하시면 됩니다.
+ * 사용하실 때 <SmallButton text="게임하기!" /> 형식으로 사용하시면 됩니다.
  */
 
 interface SmallButtonInterface {
   text: string;
 }
 
+const handleClick = () => {
+  console.log('click');
+};
+
 const SmallButton: React.FC<SmallButtonInterface> = ({ text }) => {
-  return <Button>{text}</Button>;
+  return (
+    <Button text={text} onClick={handleClick}>
+      {text}
+    </Button>
+  );
 };
 
 export default SmallButton;
 
-const Button = styled.button`
+const Button = styled.button<{ text: string }>`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 99px;
-  height: 40px;
-  border-radius: 20px;
-  font-size: 15px;
+  width: ${(props) =>
+    props.text.length > 5 ? '94px' : props.text.length > 4 ? '127px' : '93px'};
+  height: ${(props) =>
+    props.text.length > 5 ? '38px' : props.text.length > 4 ? '55px' : '38px'};
+  border-radius: 15px;
+  font-size: ${(props) => (props.text.length == 5 ? '20px' : '13px')};
+  color: ${({ theme }) => theme.colors.WHITE};
   background-color: ${({ theme }) => theme.colors.ORANGE1};
 `;
