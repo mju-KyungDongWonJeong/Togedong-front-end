92708129d075798b6856896e8c5a352bf76af910
gyuhyuk <bruno3@naver.com>
2023-10-31 16:12:14 +0900

    feat: game-result-modal 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import styled, { ThemeProvider } from 'styled-components';
 import { theme } from './styles/Theme';
 import './App.css';
-import ChallengeApply from './assets/modal/ChallengeApply';
-import SmallButton from './assets/component/SmallButton';
+
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Divv>
-        {/* <ChallengeApply /> */}
-        <SmallButton text="방 생성하기" />
-      </Divv>
-    </ThemeProvider>
-  );
+  return <ThemeProvider theme={theme}></ThemeProvider>;
 }
 
 export default App;
